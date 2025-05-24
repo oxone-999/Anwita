fbaaaad0f6c939388c091e81fbeb498db61bdbe0
anujverma999 <anuj.verma@tcgdigital.com>
2025-05-24 15:56:54 +0530

    Add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,200 +7,142 @@ function App() {
     {
       id: 1,
       dish: "Kaju Katli",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 2,
       dish: "Dark Chocolate less than 50%",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 3,
       dish: "Cheese Cake",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 4,
       dish: "Black Forest Cake",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 5,
       dish: "Banana Walnut Cake",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 6,
       dish: "Biscuit & cream flavoured Ice Cream ",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 7,
       dish: "Oreo Ice Cream",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 8,
       dish: "Coconut Icecream",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 9,
       dish: "Bournville rum and raisin",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 10,
       dish: "Oreo Flavour",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 11,
       dish: "Pani Puri (Sweet)",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 12,
       dish: "Rasmalai",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 13,
       dish: "Khatta Meetha Achar",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 14,
       dish: "Bhedki Maach",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 15,
       dish: "Rui Maach",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 16,
       dish: "Katla Maach",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 17,
       dish: "Chingri",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 18,
       dish: "Crabs",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 19,
       dish: "Pomogranade Peeled Eaten by my hands",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 20,
       dish: "Gandho Raj Lebu",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 21,
       dish: "Laal Saag",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 22,
       dish: "Cheese Carbonara Buldak",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 23,
       dish: "Bournville Dark Chocolate Fruits and Nuts",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 24,
       dish: "Elaichi Saunf Tic Tac",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 25,
       dish: "Crossiant Orange Flavoured",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 26,
       dish: "Licchi Hand Peeled",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 27,
       dish: "Allergy from Baingan",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 28,
       dish: "Doesnt Like Besan",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 29,
       dish: "Doesnt Like Mutton",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 30,
       dish: "Doesnt Like Eggs",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 31,
       dish: "Doesnt Like All-Fish",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 32,
       dish: "Mint ki diwani",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 33,
       dish: "Orbit mint sugar free",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
+    },
+    {
+      id: 34,
+      dish: "Orbit mint sugar free",
+    },
+    {
+      id: 35,
+      dish: "Papri Chaat",
     },
   ];
 
@@ -208,14 +150,10 @@ function App() {
     {
       id: 1,
       flower: "Cherry Parfait",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
     {
       id: 2,
       flower: "Sun Flowers",
-      image:
-        "https://www.cookwithmanali.com/wp-content/uploads/2018/10/Kaju-Katli-Recipe.jpg",
     },
   ];
 
@@ -225,6 +163,7 @@ function App() {
     { id: 3, gift: "cheesecake" },
     { id: 4, gift: "momos" },
     { id: 5, gift: "pani puri" },
+    { id: 6, gift: "papri chaat" },
   ];
 
   const facts = [
